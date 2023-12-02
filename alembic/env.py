ccc6b88b6123ecbe8fd6fad7b46bafb49ffbf599
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-12-02 16:42:27 +0200

    add new migrations and target_metadata in env.py

--- alembic/env.py
+++ alembic/env.py
@@ -8,6 +8,7 @@ from alembic import context
 from config import DB_HOST, DB_PORT, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD
 
 from models import model
+from auth import models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -30,7 +31,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = model.Base.metadata
+target_metadata = [model.Base.metadata, models.Base.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
